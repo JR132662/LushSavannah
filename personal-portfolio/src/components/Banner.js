e54792158a56259f8908a2f673800ca13cd1d237
JR132662 <cooljon08@gmail.com>
2023-02-18 00:15:05 +0000

    save

--- personal-portfolio/src/components/Banner.js
+++ personal-portfolio/src/components/Banner.js
@@ -55,9 +55,9 @@ export const Banner = () => {
             <TrackVisibility>
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Judy`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <span className="tagline">Software Solutions</span>
+                <h1>{`Welcome to RodTech`}</h1>
+                  <p>Welcome to our software solutions company! We are a team of skilled and experienced software developers who provide innovative and customized solutions to meet the unique needs of your business. Our services range from website design and development to mobile app development, e-commerce solutions, and cloud-based software development</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
